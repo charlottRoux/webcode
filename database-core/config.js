f43cb2da5cfdfd802e429806519e3648533a83c7
Charlotte Roux <charlottroux@gmail.com>
2022-10-20 01:41:49 -0500

    fix

--- database-core/config.js
+++ database-core/config.js
@@ -1,5 +1,5 @@
 const {MongoClient, ServerApiVersion} = require('mongodb');
-const uri = "mongodb+srv://BorphAdmin:2g2At1xYtmlPk8SW@cluster0.cxzydjb.mongodb.net/?retryWrites=true&w=majority"
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
